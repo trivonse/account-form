37d2c9a84dcf8c84e775dddf344a45d3c33fe810
Jamshid Toshov <j.toshov@smartblax.com>
2026-02-03 21:17:51 +0500

    feat: app setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import AccountList from './components/AccountList.vue'
+</script>
+
+<template>
+  <UApp>
+    <main class="app-main">
+      <AccountList />
+    </main>
+  </UApp>
+</template>
+
+<style scoped>
+  .app-main {
+    min-height: 100vh;
+    background-color: var(--ui-bg);
+  }
+</style>
